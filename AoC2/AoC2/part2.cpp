851d4c4057b247a0a5739b2193fe7e842b490d38
David Carboveanu <130596873+steepcloud@users.noreply.github.com>
2023-12-04 01:15:58 +0200

    Update part2.cpp

--- AoC2/AoC2/part2.cpp
+++ AoC2/AoC2/part2.cpp
@@ -13,7 +13,6 @@ struct Game {
     std::vector<std::vector<Cube>> subsets;
     bool addID;
 
-    // Constructor to initialize members
     Game() : id(0), sum_red(0), sum_green(0), sum_blue(0), addID(false), max_red(0), max_green(0), max_blue(0) {}
 
     void addCounts(const std::vector<Cube>& subset) {
